8c866653cda4e37c0a5a6a24ec0b81167033fab2
BennettDixon <bennettdixon16@gmail.com>
2018-09-12 23:53:05 +0000

    advanced problems, intel style assembly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point for function
+ *
+ * Description: Outputs our quote to stderr
+ * 		instead of stdout
+ *
+ * Return: 1 always (success)
+ */
+int main(void)
+{
+	char* quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", quote);
+	return (1);
+}
