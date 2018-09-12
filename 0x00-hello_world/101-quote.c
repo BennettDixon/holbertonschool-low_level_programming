74b8b46af47d6b87be46c4580d94168e6e24998b
BennettDixon <bennettdixon16@gmail.com>
2018-09-12 23:57:59 +0000

    betty formatted

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point for function
  *
  * Description: Outputs our quote to stderr
- * 		instead of stdout
+ * instead of stdout
  *
  * Return: 1 always (success)
  */
 int main(void)
 {
-	char* quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+	char *quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+
 	fprintf(stderr, "%s\n", quote);
 	return (1);
 }
