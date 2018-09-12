eff2f64aa875f0a85374ee23948256de9ca4149c
BennettDixon <bennettdixon16@gmail.com>
2018-09-12 23:51:21 +0000

    updated betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  * @void: no paramater
  *
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
