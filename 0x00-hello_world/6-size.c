61e27312b578cec540cfd09ce0fc43ee639252a5
BennettDixon <bennettdixon16@gmail.com>
2018-09-12 19:26:11 +0000

    sizes in 32 and 64 bit OS's with C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  main - Entry point
+  *
+  Description: prints sizes of different types in c
+  *
+  Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(double));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
