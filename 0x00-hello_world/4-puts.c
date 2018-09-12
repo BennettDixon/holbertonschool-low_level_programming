0b6278f7e5557eca0c46fe856c8f343127bf87c3
BennettDixon <bennettdixon16@gmail.com>
2018-09-12 19:11:31 +0000

    using puts and printf to print character arrays

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: Uses puts to output 
+ * "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char* holbertonStr = "\"Programming is like building a multilingual puzzle";
+	puts(holbertonStr);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char* grammar = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", grammar);
+	return (0);
+}
