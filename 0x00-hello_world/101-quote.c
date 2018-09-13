1698104eba6de36314a4bf045748b0409e5b3c58
BennettDixon <bennettdixon16@gmail.com>
2018-09-13 00:07:55 +0000

    fixed quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, quote, 59);
 	return (1);
