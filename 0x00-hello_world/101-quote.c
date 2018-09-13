f9be462dcd2884eeba0bc54b4ebc6fd5d942460b
BennettDixon <bennettdixon16@gmail.com>
2018-09-13 00:06:03 +0000

    using write instead of printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point for function
  *
- * Description: Outputs our quote to stderr
+ * Description: Places our quote to stderr
  * instead of stdout
  *
  * Return: 1 always (success)
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
+	char *quote = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, "%s\n", quote);
+	write(2, quote, 59);
 	return (1);
 }
