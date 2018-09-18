83c9742d3be819ffd81ae67ba1de691d45ce99cf
BennettDixon <bennettdixon16@gmail.com>
2018-09-18 19:19:25 +0000

    lots of nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints 00:00 - 23:59
+ *
+ * Return: void (always)
+ */
+void jack_bauer(void)
+{
+	int hourTens, hourOnes, minutesTens, minutesOnes;
+
+	hourTens = 0;
+	while (hourTens <= 2)
+	{
+		hourOnes = 0;
+		while (hourOnes <= 3)
+		{
+			minutesTens = 0;
+			while (minutesTens <= 5)
+			{
+				minutesOnes = 0;
+				while (minutesOnes <= 9)
+				{
+					_putchar(hourTens + '0');
+					_putchar(hourOnes + '0');
+					_putchar(':');
+					_putchar(minutesTens + '0');
+					_putchar(minutesOnes + '0');
+					_putchar('\n');
+					minutesOnes++;
+				}
+				minutesTens++;
+			}
+			hourOnes++;
+		}
+		hourTens++;
+	}
+}
