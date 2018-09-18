61f14c3323898675e7def1268baef414ffa6b60e
BennettDixon <bennettdixon16@gmail.com>
2018-09-18 16:45:30 +0000

    added betty-doc comment

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,14 @@
-void print_alphabet_x10(void);
-
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: always void
+ */
 void print_alphabet_x10(void)
 {
 	int i;
 
 	i = 0;
-	
+
 	while (i < 10)
 	{
 		int ch = 'a';
