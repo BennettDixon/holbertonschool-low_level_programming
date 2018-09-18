ebc4560aaabb6f240660b172e50cc887203ce34f
BennettDixon <bennettdixon16@gmail.com>
2018-09-18 21:38:07 +0000

    lol I could use stdlib

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints n..98
  *
@@ -8,34 +8,13 @@
  */
 void print_to_98(int n)
 {
-	int p1, p2, p3, tempN;
-
 	while (n >= 98 || n < 98)
 	{
-		if (n < 0)
-			_putchar('-');
-		tempN = n;
-		p3 = tempN % 10;
-		tempN = tempN / 10;
-		p2 = tempN % 10;
-		tempN = tempN / 10;
-		p1 = tempN % 10;
-		if (n <= 0)
-		{
-			p1 = p1 * -1;
-			p2 = p2 * -1;
-			p3 = p3 * -1;
-		}
-
-		_putchar((p1 == 0) ? 0 : p1 + '0');
-		_putchar((p1 == 0 && p2 == 0) ? 0 : p2 + '0');
-		_putchar(p3 + '0');
-
+		printf("%d", n);
 		if (n == 98)
 			break;
-		_putchar(',');
-		_putchar(' ');
+		printf(", ");
 		(n > 98) ? n-- : n++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
