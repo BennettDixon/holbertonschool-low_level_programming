e431507df87017b85bb07f4e480befa154e6a108
BennettDixon <bennettdixon16@gmail.com>
2018-09-18 20:33:09 +0000

    printing out times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * times_table prints out 0..9 times table
+ *
+ * Return: always void
+ */
+void times_table(void)
+{
+	int i, n, pTens, pOnes, lineTotal;
+
+	i = 0;
+	pTens = 0;
+	pOnes = 0;
+	while (i < 10)
+	{
+		n = 0;
+		lineTotal = 0;
+		while (n < 11)
+		{
+			lineTotal = n * i;
+
+			pOnes = lineTotal % 10; /* grab last digit */
+			pTens = (lineTotal - pOnes) / 10; 
+			/* ^^grab first digit of two digit number^^ */
+			if (pTens == 0 && n > 0)
+				_putchar(' ');
+			else if (n > 0)
+				_putchar(pTens + '0');
+			_putchar(pOnes + '0');
+			if (n == 10)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
