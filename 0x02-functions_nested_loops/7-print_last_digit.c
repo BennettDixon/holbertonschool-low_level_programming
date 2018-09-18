1357d4080fd0bebaa3f85a56aaa7876e8edde74c
BennettDixon <bennettdixon16@gmail.com>
2018-09-18 17:31:33 +0000

    changed logic of putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,10 @@ int print_last_digit(int n)
 {
 	int r;
 
-	if (n < 0)
-		n = (-(n));
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
 	r = n % 10;
-	_putchar(r + '0');
-	return (r);
+	if (r < 0)
+		_putchar(r * -1 + '0');
+	else
+		_putchar(r + '0');
+	return (-1 * r);
 }
