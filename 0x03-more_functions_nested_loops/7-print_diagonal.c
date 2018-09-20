1e30eeada16d2f9bab8dcffb8021b59c2fe768db
BennettDixon <bennettdixon16@gmail.com>
2018-09-20 16:12:39 +0000

    printing diagonal line nested loops

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints a diagonal line n long
+ *
+ * @n: length of line
+ *
+ * Return: always void
+ */
+void print_diagonal(int n)
+{
+	int i, spaceC;
+
+	i = 0;
+	while (n > 0 && i < n)
+	{
+		spaceC = 0;
+		while (spaceC < i)
+		{
+			_putchar(' ');
+			spaceC++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
