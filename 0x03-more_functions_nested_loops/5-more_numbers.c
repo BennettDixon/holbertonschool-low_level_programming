d12f4f7f0f3ba9fc78c6162254e5f6127980c497
BennettDixon <bennettdixon16@gmail.com>
2018-09-20 19:56:23 +0000

    fixed output so no null char outputted

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,9 @@ void more_numbers(void)
 		n = 0;
 		while (n <= 14)
 		{
-			_putchar((n < 10) ? 0 : n / 10 + '0');
-			_putchar(n % 10 + '0');
+			if (n >= 10)
+				_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 			n++;
 		}
 		_putchar('\n');
