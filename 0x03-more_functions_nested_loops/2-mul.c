ad68d28d7a22bcb3a29a88e1dc1aefc8975da6eb
BennettDixon <bennettdixon16@gmail.com>
2018-09-20 15:40:07 +0000

    multiplying function

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiply two nums together
+ *
+ * @a: integer to multiply by b
+ * @b: integer to multiply by a
+ *
+ * Return: result of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
