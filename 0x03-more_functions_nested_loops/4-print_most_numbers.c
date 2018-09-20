aee1f37c1152a72e429c10350df000151b70d528
BennettDixon <bennettdixon16@gmail.com>
2018-09-20 15:45:20 +0000

    print nums, skipping some

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_numbers - print numbers 0..9\n
+ *
+ * Description: skips 2 & 4
+ *
+ * Return: always void
+ */
+void print_most_numbers(void)
+{
+	int i = 48; /* ascii: 0 */
+
+	while (i <= 57) /* ascii: 9 */
+	{
+		if (i == 50 || i == 52) /* skip 2 & 4 */
+		{
+			i++;
+			continue;
+		}
+		_putchar(i++);
+	}
+	_putchar('\n');
+}
