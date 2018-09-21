37ddc4cc75370a8e16db9d8c197374aa2ce06aba
BennettDixon <bennettdixon16@gmail.com>
2018-09-21 21:13:32 +0000

    minor code length improvements

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -10,23 +10,20 @@ void print_triangle(int size)
 {
 	int i, n, spaceC;
 
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
 	i = 0;
 	spaceC = size - 1;
 	while (i < size)
 	{
 		n = 0;
 		while (n < size)
-		{
-			if (n < spaceC)
-				_putchar(' ');
-			else
-				_putchar('#');
-			n++;
-		}
+			_putchar((n++ < spaceC) ? ' ' : '#');
 		_putchar('\n');
 		spaceC--;
 		i++;
 	}
-	if (size < 1)
-		_putchar('\n');
 }
