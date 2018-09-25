97d1a8892520c83590487a58709f59ae4852696d
BennettDixon <bennettdixon16@gmail.com>
2018-09-25 18:52:59 +0000

    reversing strings with pointers

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * rev_string - reverses given string
+ *
+ * @s: string to reverse
+ *
+ * Return: always void
+ */
+void rev_string(char *s)
+{
+	char *end = s;
+	char store;
+
+	while (*end)
+		end++; /* set pos of end pointer to point to last element */
+	end = end - 1; /* set end pos to end -1 b/c end is really '\0' */
+	while (s < end) /* stop once we get to last address */
+	{
+		store = *s; /* set store ch to s[currentPos] */
+		*s++ = *end; /* set position of s = pos end, and increment s */
+		*end-- = store; /* set pos of end to stored char and increment */
+	}
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -15,3 +15,4 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
