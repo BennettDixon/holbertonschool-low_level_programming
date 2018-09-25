f962906ce12f6b114533951fdd0d45a6939d2f82
BennettDixon <bennettdixon16@gmail.com>
2018-09-25 19:26:46 +0000

    printing half of a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts_half - print second half of str
+ *
+ * @str: string to cut in half & print
+ *
+ * Return: always void
+ */
+void puts_half(char *str)
+{
+	int length, charsToPrint = 0;
+
+	while (*(str + length) != '\0')
+		length++;
+	if (!(length % 2)) /* if uneven number */
+		charsToPrint = (length - 1) / 2;
+	else
+		charsToPrint = length / 2;
+	charsToPrint++; 
+	while (charsToPrint < length)
+		_putchar(*(str + charsToPrint++));
+	_putchar('\n');
+}
