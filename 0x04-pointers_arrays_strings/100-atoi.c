52f1e1e6cbc803a1b3bc59b8c1d8f2e77784bffa
BennettDixon <bennettdixon16@gmail.com>
2018-09-26 17:18:15 +0000

    fixed negative case

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,27 @@
  */
 int _atoi(char *s)
 {
-	char sign = 0;
-	int total = 0;
+	int sign = 1;
+	unsigned int total = 0;
 	char working = 0;
 
 	while (*s)
 	{
+		if (*s == '-')
+			sign = sign * -1;
 		if (*s >= '0' && *s <= '9')
 		{
 			working = 1;
-			if (sign == 0)
-			{
-				sign = *(s - 1);
-				if (sign != '-' && sign != '+')
-					sign = '+';
-			}
 			total = total * 10 + *s - '0';
 		}
 		else if (*s < '0' || *s > '9')
 		{
-			if (working) 
+			if (working)
 				break;
 		}
 		s++;
 	}
-	if (sign == '-')
+	if (sign < 0)
 		total = (-(total));
 	return (total);
 }
