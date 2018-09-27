52661f06bca184f3aabd022df4c48041122edd67
BennettDixon <bennettdixon16@gmail.com>
2018-09-27 21:58:34 +0000

    reversing arrays

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: an array of ints
+ * @n: size of array
+ *
+ * Return: always void
+ */
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int store;
+
+	while (start < n - 1)
+	{
+		store = a[n - 1];
+		a[n - 1] = a[start];
+		a[start] = store;
+		n--;
+		start++;
+	}	
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -23,3 +23,6 @@ char *_strcpy(char *dest, char*src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
