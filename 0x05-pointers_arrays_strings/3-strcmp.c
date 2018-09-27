285d7d82ac78a4c2e3a9fae0339a90f84cc05da7
BennettDixon <bennettdixon16@gmail.com>
2018-09-27 20:28:22 +0000

    string comparison using ptrs

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: -15 if s1 < s2
+ *          0  if s1 = s2
+ *          15 if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1Length = 0, s2Length = 0;
+
+	while (s1[s1Length] != '\0')
+		s1Length++;
+	while (s2[s2Length] != '\0')
+		s2Length++;
+
+	if (s1Length < s2Length)
+		return (-15);
+	else if (s1Length > s2Length)
+		return (15);
+	return (0); /* equal */
+}
