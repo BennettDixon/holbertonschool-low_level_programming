ca2c62939c688691f3ac1f624fad38dc0bea16ce
BennettDixon <bennettdixon16@gmail.com>
2018-09-27 20:56:27 +0000

    added case for same strlen diff str

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,22 @@ int _strcmp(char *s1, char *s2)
 {
 	int s1Length = 0, s2Length = 0;
 
-	while (*s1++)
+	if (*s1 == *s2)
+		return (0); /* don't do unneeded processing if = */
+	while (*(s1 + s1Length != '\0'))
 		s1Length++;
-	while (*s2++)
+	while (*(s2 + s2Length) != '\0')
 		s2Length++;
 
 	if (s1Length < s2Length)
 		return (-15);
-	else if (s1Length > s2Length)
+	if (s1Length > s2Length)
 		return (15);
+	
+	while (*s1)
+		if (*s1 < *s2)
+			return (-15);
+		if (*s1 > *s2)
+			return (15);
 	return (0); /* equal */
 }
