f471b9eec92e44495ac9c3656d553722d1726526
BennettDixon <bennettdixon16@gmail.com>
2018-09-28 01:13:40 +0000

    initial commits with debugs

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,3 +1,4 @@
+#include <stdio.h> /* debugging use */
 /**
  * infinite_add - adds two numbers
  *
@@ -8,5 +9,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	printf("first number:%s\nsecond number:%s\nbuff:%s\nsize:%d",
+		n1, n2, r, size_r);
 	return ("0");
 }
---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_buffer - prints contents of a buffer
+ *
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: always void
+ */
+void print_buffer(char *b, int size)
+{
+	printf("buffer: %s\nsize: %d\n", b, size);
+}
