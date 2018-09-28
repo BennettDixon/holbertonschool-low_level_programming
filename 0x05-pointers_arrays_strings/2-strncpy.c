1c6dfd2116aacd179cd1b588ad86c4f5b574c083
BennettDixon <bennettdixon16@gmail.com>
2018-09-28 04:30:11 +0000

    initialized srcLength

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *temp = dest;
-	int counter = 0, srcLength;
+	int counter = 0, srcLength = 0;
 
 	if (n <= 0)
 		return (dest);
 	while (src[srcLength] != '\0')
 		srcLength++;
-
+	srcLength++;
 	if (n > srcLength)
 		n = srcLength;
 
