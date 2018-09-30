f65c9e1a6379f32a0102be91b4edd4ce25ec0834
BennettDixon <bennettdixon16@gmail.com>
2018-09-30 21:38:43 +0000

    working string adding infinite size as long as buffer supports

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,4 +1,5 @@
 #include <stdio.h> /* debugging use */
+#define FILL 100 /* fill with 'd' */
 /**
  * infinite_add - adds two numbers
  *
@@ -11,7 +12,65 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	printf("first number:%s\nsecond number:%s\nbuff:%s\nsize:%d",
-		n1, n2, r, size_r);
-	return ("0");
+	int tempSizeR = size_r;
+	int bufferPlacer = 0;
+	int n1Length = 0, n2Length = 0;
+	int result = 0, carryStore = 0;
+
+	while (*(n1 + n1Length) != '\0')
+		n1Length++;
+	while (*(n2 + n2Length) != '\0')
+		n2Length++;
+
+	/* account for 9999, 1, size_r:4, and extra space needed for '\0' */
+	if (n1Length > size_r - 2 || n2Length > size_r - 2)
+		return (0);
+	n1Length--, n2Length--; /* set element behind null byte */
+
+	while (size_r >= 0)
+	{	
+		if (size_r == tempSizeR) /* set last byte to null byte */
+		{
+			r[size_r--] = '\0';
+			continue;
+		}
+		if (n1Length < 0 && n2Length < 0 && carryStore == 0)
+		{
+			r[size_r--] = FILL;
+			continue; /* fill in rest with constants's */
+		}
+		else if (n1Length < 0 && n2Length < 0 && carryStore != 0)
+			result = carryStore;
+		else if (n1Length < 0)
+			result = n2[n2Length] + carryStore - '0';
+		else if (n2Length < 0)
+			result = n1[n1Length] + carryStore - '0';
+		else /* normal case */
+		{
+			result = (n1[n1Length] - '0') + (n2[n2Length] - '0') + carryStore;
+		}
+		carryStore = 0;
+		if (result > 9)
+		{
+			carryStore = 1;
+			result = result - 10;
+		}
+		n1Length--, n2Length--;
+		r[size_r--] = result + '0';
+	}
+	/* below loop moves elements to beginning of array */
+	size_r = 0;
+	while (size_r < tempSizeR)
+	{
+		if (r[size_r] != FILL)
+		{
+			r[bufferPlacer++] = r[size_r];
+			r[size_r] = '\0';
+		}
+		else
+			r[size_r] = '\0';
+		size_r++;
+	}
+
+	return (r);
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -31,3 +31,4 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
