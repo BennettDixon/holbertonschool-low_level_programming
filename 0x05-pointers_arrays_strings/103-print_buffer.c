1655f53ba33f44d2ffec54bae9df85ce2b987666
BennettDixon <bennettdixon16@gmail.com>
2018-10-01 00:44:11 +0000

    creating my own buffer print function

--- 0x05-pointers_arrays_strings/103-print_buffer.c
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print_hex_line(char *buffer, int numBitsInLine, int currentPosition);
+void print_buffer_line(char *b, int n, int cur);
 /**
  * print_buffer - prints contents of a buffer
  *
@@ -10,5 +12,57 @@
  */
 void print_buffer(char *b, int size)
 {
-	printf("buffer: %s\nsize: %d\n", b, size);
+	int quo, rem, i = 0;
+	int bitCounter = 0, numBitsInLine;
+	int nestedCounter = 0;
+
+	quo = size / 10;
+	rem = size % 10;
+	if (rem)
+		quo++;
+	while (i < quo) /* loop through 10 bits at a time */
+	{
+		numBitsInLine = (size - rem) > bitCounter ? 10 : rem;
+		printf("%.8x: ", bitCounter);
+		print_hex_line(b, numBitsInLine, bitCounter);
+		print_buffer_line(b, numBitsInLine, bitCounter);
+		putchar('\n');
+		bitCounter += 10;
+		i++;
+	}
+}
+
+void print_hex_line(char *b, int numBitsInLine, int currentPos)
+{
+	int nestedCounter = 0;
+
+	while (nestedCounter < 10)
+	{
+		if (nestedCounter >= numBitsInLine)
+			printf("  ");
+		else
+			printf("%.2x", b[currentPos + nestedCounter]);
+		if (nestedCounter % 2)
+			putchar(' ');
+		nestedCounter++;
+	}
+}
+
+void print_buffer_line(char *b, int numBitsInLine, int currentPos)
+{
+	int nestedCounter = 0;
+
+	while (nestedCounter < 10)
+	{
+		if (nestedCounter >= numBitsInLine)
+			break;
+		if (b[currentPos + nestedCounter] >= 0x20
+			&& b[currentPos + nestedCounter] <= 0x7f)
+		{
+			printf("%c", b[currentPos + nestedCounter]);
+		}
+		else
+			printf(".");
+		nestedCounter++;
+	}
 }
