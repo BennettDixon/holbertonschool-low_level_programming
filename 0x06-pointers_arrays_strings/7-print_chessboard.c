1a646fa96a066cae516c491caef3c28524716896
BennettDixon <bennettdixon16@gmail.com>
2018-10-01 17:36:45 +0000

    printing multi-dimensional arrays

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints out a chessboard
+ *
+ * @a: chessboard to print, 2D array [8] columns
+ *
+ * Return: always void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int subC, primC = 0;
+
+	while (primC < 8)
+	{
+		subC = 0;
+		while (subC < 8)
+			_putchar(a[primC][subC++]);	
+		_putchar('\n');
+		primC++;
+	}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -35,3 +35,7 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
