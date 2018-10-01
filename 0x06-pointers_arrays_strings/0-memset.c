8e2e45bf1e5d687cb7dedacf7110c746a309a50c
BennettDixon <bennettdixon16@gmail.com>
2018-10-01 16:25:37 +0000

    setting memory bytes

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ * _memset - fills n bytes of memory
+ *
+ * @s: pointer to start of memory area
+ * @b: constant byte to fill with
+ * @n: num bytes to fill in
+ *
+ * Return: pointer to beginning of memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *start = s;
+
+	while (i < n)
+	{
+		*s++ = b;
+		i++;
+	}
+	return (start);
+}
