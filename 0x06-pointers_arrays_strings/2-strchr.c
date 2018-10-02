62f36b8c5525562e153e6c453a525b5a8bd8a3ea
BennettDixon <bennettdixon16@gmail.com>
2018-10-02 16:52:13 +0000

    testing checker

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -14,8 +14,10 @@ char *_strchr(char *s, char c)
 	while (*(s + i))
 	{
 		if (*(s + i) == c)
-			return (*(s + i));
+			return (s + i);
 		i++;
 	}
+	if (*(s + i) == '\0')
+		return ('\0');
 	return (NULL);
 }
