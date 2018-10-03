659fd4efa213ad823bac9e3b84f28cfb896d72e1
BennettDixon <bennettdixon16@gmail.com>
2018-10-03 20:17:47 +0000

    betty-doc and styling

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -9,15 +9,24 @@ int recursive_check(char *s1, char *s2, int wildC);
  */
 int wildcmp(char *s1, char *s2)
 {
-	return recursive_check(s1, s2, 0);
+	return (recursive_check(s1, s2, 0));
 }
+/**
+ * recursive_check - checks for wild cards recursively
+ *
+ * @s1: string 1 to check (no wild cards)
+ * @s2: string to to check (can contain wildcards)
+ * @wildC: counter to see if wildcard is present
+ *
+ * Return: 1 if identical, 0 for not, -1 for error
+ */
 int recursive_check(char *s1, char *s2, int wildC)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-	{	
+	{
 		return (1);
 	}
-	if (*s2 == '*') 
+	if (*s2 == '*')
 		return (recursive_check(s1, s2 + 1, ++wildC));
 	else if (*s1 != *s2 && wildC > 0) /* skip to next rel char */
 	{
