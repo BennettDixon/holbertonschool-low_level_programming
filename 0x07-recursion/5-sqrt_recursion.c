10921c803aacae893e496ab3ae7f5d7eadedfd83
BennettDixon <bennettdixon16@gmail.com>
2018-10-03 17:13:43 +0000

    finding square roots with recursion

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+int _find_sqrt(int prev, int find);
+/**
+ * _sqrt_recursion - finds square root of n, recursively
+ *
+ * @n: int to find sqroot from
+ *
+ * Return: sqroot, -1 if no natural sqroot for n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0) /* error case */
+		return (-1);
+	else if (n == 0) /* 0 case */
+		return (0);
+	return (_find_sqrt(n, n));
+}
+/**
+ * _find_sqrt - finds square root recursively, needs prev param
+ *
+ * @prev: previous result of function
+ * @find: constant int to find sq root for
+ *
+ * Return: square root of find, or -1 if not found
+ */
+int _find_sqrt(int prev, int find)
+{
+	if (prev <= 0) /* didn't find a nr, there was remainder */
+		return (-1);
+	if (prev * prev == find) /* natural root found */
+		return (prev);
+	return (_find_sqrt(prev - 1, find)); /* not super efficient */
+}
