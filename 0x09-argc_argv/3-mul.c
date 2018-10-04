76837c36496a30c9376ca67034bcf47aedd83cdb
BennettDixon <bennettdixon16@gmail.com>
2018-10-04 16:41:50 +0000

    multiplying numbers with command line args

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point for program
+ *
+ * @argC: count of args present
+ * @argV: array of char * pointing to args
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+		product *= atoi(argv[i]);
+	printf("%d\n", product);
+	return (0);
+}
