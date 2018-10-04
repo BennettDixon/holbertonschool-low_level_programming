a047ee5b951b6665fb9b0948601b137bc25e6001
BennettDixon <bennettdixon16@gmail.com>
2018-10-04 16:50:55 +0000

    adding only integers, error handling, argc & argv

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point for program
+ *
+ * @argC: count of args present
+ * @argV: array of char * pointing to args
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i])) /* non numeric */
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
