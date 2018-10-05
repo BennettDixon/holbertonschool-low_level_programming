897bbf52e16355e9a95681294c46a836dcc234b2
BennettDixon <bennettdixon16@gmail.com>
2018-10-05 17:09:07 +0000

    optimized algorithm

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,4 +1,6 @@
 int prime_checker(int n, int prevCheck);
+int _find_sqrt(int prev, int find);
+int _sqrt_recursion(int n);
 /**
  * is_prime_number - checks if n is a prime number
  *
@@ -10,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1) /* edge case */
 		return (0);
-	return (prime_checker(n, n - 1));
+	return (prime_checker(n, _sqrt_recursion(n)));
 }
 /**
  * prime_checker - checks if n is prime number using recursion
@@ -29,4 +31,33 @@ int prime_checker(int n, int prevCheck)
 	if (n % prevCheck == 0)
 		return (0);
 	return (prime_checker(n, prevCheck - 1));
+}
+/**
+ * _sqrt_recursion - finds square root of n, recursively
+ *
+ * @n: int to find sqroot from
+ *
+ * Return: sqroot, -1 if no natural sqroot for n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0) /* error case */
+		return (-1);
+	return (_find_sqrt(1, n));
+}
+/**
+ * _find_sqrt - finds square root recursively, needs prev param
+ *
+ * @prev: previous result of function
+ * @find: constant int to find sq root for
+ *
+ * Return: square root of find, or -1 if not found
+ */
+int _find_sqrt(int prev, int find)
+{
+	if (prev > find) /* didn't find a nr, there was remainder */
+		return (-1);
+	if (prev * prev == find) /* natural root found */
+		return (prev);
+	return (_find_sqrt(prev + 1, find)); /* not super efficient */
 }
