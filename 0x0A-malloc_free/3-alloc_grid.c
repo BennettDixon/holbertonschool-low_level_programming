db794c5ae6544238ad949432036276fd6a0c695b
BennettDixon <bennettdixon16@gmail.com>
2018-10-09 20:26:58 +0000

    added edge case for NULL return from malloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -9,11 +9,13 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **arr, i = 0, n = 0;	
+	int **arr, i = 0, n = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
 	while (i < height)
 		arr[i++] = malloc(sizeof(int) * width);
 	for (i = 0; i < height; i++)
