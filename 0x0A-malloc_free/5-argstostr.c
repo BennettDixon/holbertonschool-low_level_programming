2d06a4977e182f357e3d56d01c7ecc3dba92db2e
BennettDixon <bennettdixon16@gmail.com>
2018-10-09 17:02:52 +0000

    concatenating args using malloc

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+int _strLen(char *);
+/**
+ * argstostr - takes all args and concatenates them into str
+ *
+ * @ac: arg count, num of args in av
+ * @av: array of pointers to words (args)
+ *
+ * Return: pointer to beginning of new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, n = 0, totalSize;
+	char *newStr, *startNewStr;
+
+	while (i < ac)
+	{
+		totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
+		i++;
+	}
+	newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
+	startNewStr = newStr;
+
+	i = 0;
+	while (i < ac)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			*newStr++ = av[i][n];
+		}
+		*newStr++ = '\n';
+		i++;
+	}
+	*newStr = '\0';
+	return (startNewStr);
+}
+/**
+ * _strLen - returns length of string
+ *
+ * @s: string to check
+ *
+ * Return: integer, length of string
+ */
+int _strLen(char *s)
+{
+	int strL = 0;
+
+	while (*(s + strL))
+		strL++;
+	return (strL);
+}
