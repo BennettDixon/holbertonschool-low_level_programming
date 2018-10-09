aef03e82cf0041139354ff3ba7a9226f83c291fb
BennettDixon <bennettdixon16@gmail.com>
2018-10-09 19:20:29 +0000

    creating array of words from given string

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+int get_word_length(char *str);
+int get_word_count(char *str);
+char *get_next_word(char *str);
+/**
+ * strtow - takes a string and seperates its words
+ *
+ * @str: string to seperate into words
+ *
+ * Return: 2D array of pointers to each word
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int wc, wordLen, n, i = 0;
+
+	wc = get_word_count(str);
+	words = malloc((wc + 1) * sizeof(char *));
+	while (i < wc)
+	{
+		wordLen = get_word_length(str);
+		words[i] = malloc(wordLen * sizeof(char) + 1);
+		n = 0;
+		while (n < wordLen)
+		{
+			words[i][n] = *(str + n);
+			n++;
+		}
+		words[i][n] = '\0'; /* set end of str */
+		str = get_next_word(str);
+		i++;
+	}
+	words[i] = NULL; /* last element is null for iteration */
+	return (words);
+}
+/**
+ * get_word_length - gets the word length of cur word in str
+ *
+ * @str: string to get word length from current word
+ *
+ * Return: word length of current word
+ */
+int get_word_length(char *str)
+{
+	int wLen = 0;
+
+	while (*(str + wLen) != ' ' && *(str + wLen) != '\0')
+		wLen++;
+	return (wLen);
+}
+/**
+ * get_word_count - gets the word count of a string
+ *
+ * @str: string to get word count from
+ *
+ * Return: the word count of the string
+ */
+int get_word_count(char *str)
+{
+	int wc = 0, pending = 1; /* default 1 for 1st word */
+
+	while (*str)
+	{
+		if (*str == ' ')
+			pending = 1;
+		else if (*str != ' ' && pending)
+		{
+			pending = 0;
+			wc++;
+		}
+		str++;
+	}
+	return (wc);
+}
+/**
+ * get_next_word - gets the next word in a string
+ *
+ * @str: string to get next word from
+ *
+ * Return: pointer to first char of next word
+ */
+char *get_next_word(char *str)
+{
+	int pending = 0;
+
+	while (*str)
+	{
+		if (*str == ' ')
+			pending = 1;
+		if (*str != ' ' && pending)
+			break;
+		str++;
+	}
+	return (str);
+}
