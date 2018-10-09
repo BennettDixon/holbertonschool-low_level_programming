d772008fd900ad22f22492007e6a1be02cb50b86
BennettDixon <bennettdixon16@gmail.com>
2018-10-09 19:49:18 +0000

    null pointer check after malloc

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,7 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i = 0;
 
+	if (size <= 0)
+		return (NULL);
 	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
 	while (i < size)
 	{
 		*(arr + i) = c;
