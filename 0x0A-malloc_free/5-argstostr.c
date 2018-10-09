97faf834e00a5cfbfc8aa80714cdd9fd3ffd5a11
BennettDixon <bennettdixon16@gmail.com>
2018-10-09 17:04:51 +0000

    added edge case

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -14,12 +14,17 @@ char *argstostr(int ac, char **av)
 	int i = 0, n = 0, totalSize;
 	char *newStr, *startNewStr;
 
+	if (ac <= 0 || av == NULL)
+		return (NULL);
+
 	while (i < ac)
 	{
 		totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
 		i++;
 	}
 	newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
+	if (newStr == NULL) /* ran out of memory */
+		return (NULL);
 	startNewStr = newStr;
 
 	i = 0;
