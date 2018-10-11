073a0cd1448a19beb0284ab600995e6017709a82
BennettDixon <bennettdixon16@gmail.com>
2018-10-11 16:45:40 +0000

    created own calloc statement with memset

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+void *set_mem(void *p, unsigned int nmemb, int ch);
+/**
+ * _calloc - allocates memory for an array, and inits
+ *
+ * @nmemb: number of members in array
+ * @size: size of each member
+ *
+ * Return: void pointer to beginning of alloc/init'ed memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret;
+
+	if (nmemb < 1 || size < 1)
+		return (NULL);
+	ret = malloc(nmemb * size);
+	if (ret == NULL)
+		return (NULL);
+	ret = set_mem(ret, nmemb, 0);
+	return (ret);
+}
+/**
+ * set_mem - sets memory for void pointer
+ *
+ * @p: pointer to beginning of memory to set
+ * @nmemb: number of members of array
+ * @ch: character to set memory to
+ *
+ * Return: void pointer to beginning of set memory
+ */
+void *set_mem(void *p, unsigned int nmemb, int ch)
+{
+	char *cast = p;
+	unsigned int i = 0;
+
+	while (i < nmemb)
+		cast[i++] = ch;
+	return (p);
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -61,4 +61,5 @@ char *argstostr(int ac, char **av);
 char **strtow(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif /* HOLBERTON_H */
