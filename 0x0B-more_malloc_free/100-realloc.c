2b563c63580b506a4e4b82071588278416e31902
BennettDixon <bennettdixon16@gmail.com>
2018-10-11 19:50:20 +0000

    fixed overflow case in reassignment loop

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -41,6 +41,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(temp);
 		return (NULL);
 	}
+	if (old_size > new_size)
+		old_size = new_size;
 	for (i = 0; i < old_size; i++)
 		tempP[i] = temp[i];
 	free(temp);
