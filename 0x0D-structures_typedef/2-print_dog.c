134982e566ae7d314e43a194b94668c55d9be03f
BennettDixon <bennettdixon16@gmail.com>
2018-10-15 18:47:05 +0000

    printing contents of a struct

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+#define NILL "(nil)"
+/**
+ * print_dog - prints contents of struct dog
+ *
+ * @dog: pointer to struct of type struct dog
+ *
+ * Return: always void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = NILL;
+	if (d->owner == NULL)
+		d->owner = NILL;
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
