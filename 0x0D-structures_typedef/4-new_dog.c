2e527704b213fef683ebb1408c5c9cb64ae3b4c9
BennettDixon <bennettdixon16@gmail.com>
2018-10-15 19:17:47 +0000

    creating new structures in memory with malloc

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,69 @@
+#include "dog.h"
+#include <stdlib.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+/**
+ * new_dog - makes a new dog, memory and all
+ * @name: name to create mem and assign to for dog
+ * @age: age to assign to for dog
+ * @owner: owner to create mem and assign to for dog
+ *
+ * Return: pointer to new dog_t (struct dog)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+	new_d->name = malloc(_strlen(name) + 1);
+	if (new_d->name == NULL)
+	{
+		free(new_d);
+		return (NULL);
+	}
+	new_d->owner = malloc(_strlen(owner) + 1);
+	if (new_d->owner == NULL)
+	{
+		free(new_d->name);
+		free(new_d);
+		return(NULL);
+	}
+
+	new_d->name = _strcpy(new_d->name, name);
+	new_d->owner = _strcpy(new_d->owner, owner);
+	new_d->age = age;
+	return (new_d);
+}
+/**
+ * _strlen - gets len of str
+ * @s: string to get length of
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * _strcpy - copies string from src to dest
+ *
+ * @dest: pointer to destination of string
+ * @src: pointer to source string to copy from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*src)
+		*temp++ = *src++;
+	temp = '\0';
+	return (dest);
+}
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -17,4 +17,6 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* _DOG_H_ */
