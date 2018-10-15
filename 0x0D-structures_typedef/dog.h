1cdf7945a509f8ed9020f458f6d60fbaa587626d
BennettDixon <bennettdixon16@gmail.com>
2018-10-15 18:50:59 +0000

    typedef for structs

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,5 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* _DOG_H_ */
