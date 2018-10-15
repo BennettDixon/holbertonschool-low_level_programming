e0fe0011443df9f38d36be63544e4e9d310bd606
BennettDixon <bennettdixon16@gmail.com>
2018-10-15 20:05:59 +0000

    freeing memory in structs

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees a dog structure
+ *
+ * @d: pointer to struct to free
+ *
+ * Return: always void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
