cb4ef5a3440f24a0880d08cc023721d0fbca2099
BennettDixon <bennettdixon16@gmail.com>
2018-10-17 17:29:56 +0000

    using a function pointer as a parameter for a function

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - prints a name given a function
+ *
+ * @name: pointer to name to print
+ * @f: function to print name
+ *
+ * Return: always void
+ */
+void print_name(char *name, void (*f)(char *name))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _FUNC_POINTERS_
+#define _FUNC_POINTERS_
+void print_name(char *name, void (*f)(char *));
+#endif /* _FUNC_POINTERS_ */
