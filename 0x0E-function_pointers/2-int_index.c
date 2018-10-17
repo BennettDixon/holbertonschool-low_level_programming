604ecb63fcd1ee9ec72fadaf389468f22053924c
BennettDixon <bennettdixon16@gmail.com>
2018-10-17 19:38:27 +0000

    added null case for pointers

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * int_index - gets the index of an array element
  * @array: array to search through
@@ -11,6 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
 		return (-1);
 	while (i < size)
 	{
