7d132efe16ebde874d65803f442f3c490b4b21e5
BennettDixon <bennettdixon16@gmail.com>
2018-10-17 18:06:37 +0000

    iterating over arrays and performing actions (functions) on them via func pointers

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * array_iterator - iterates over an array and performs an action on it
+ * @array: array to iterate through
+ * @size: number of elements of array
+ * @action: action (function) to perform on array elements
+ *
+ * Return: always void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int param))
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef _FUNC_POINTERS_
 #define _FUNC_POINTERS_
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* _FUNC_POINTERS_ */
