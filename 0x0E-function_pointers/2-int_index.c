8ac162a7d5b44d43631319e1fce0c2e3adee4131
BennettDixon <bennettdixon16@gmail.com>
2018-10-17 18:14:17 +0000

    searching through arrays given comparison function via pointer

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+/**
+ * int_index - gets the index of an array element
+ * @array: array to search through
+ * @size: size of array (num of elements)
+ * @cmp: comparison function to determine if element found
+ *
+ * Return: integer representing index of element found in array
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define _FUNC_POINTERS_
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* _FUNC_POINTERS_ */
