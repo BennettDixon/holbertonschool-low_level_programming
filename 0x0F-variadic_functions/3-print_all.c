8050050020aeba0974aee8cf9723f1ca223e9ac9
BennettDixon <bennettdixon16@gmail.com>
2018-10-19 21:18:11 +0000

    fixed issue, moved va_arg into print function for proper type instead of trying to do it genericly. Now accepts va_list instead of void *

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,15 +1,11 @@
 #include <stdarg.h>
 #include <stdio.h>
-typedef struct print_convert_variable
-{
-	char type;
-	void (*f)(void *);
-} conv_t;
-void _print_char(void *ch);
-void _print_integer(void *i);
-void _print_float(void *f);
-void _print_string(void *s);
-void (*get_print_func(char ch))(void *);
+#include "variadic_functions.h"
+void _print_char(va_list *args);
+void _print_integer(va_list *args);
+void _print_float(va_list *args);
+void _print_string(va_list *args);
+void (*get_print_func(char ch))(va_list *args);
 /**
  * print_all - prints all parameters passed variadiclly
  * @format: string with types of variables, in order
@@ -20,26 +16,32 @@ void (*get_print_func(char ch))(void *);
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	void *arg;
-	void (*print_f)(void *);
+	void (*print_f)(va_list *);
 	unsigned int i = 0;
+	char *seperator = "";
 
 	va_start(args, format);
 	while ((*(format + i)))
 	{
-		arg = va_arg(args, void *);
-		printf("char in loop:%c\n", *(format + i));
 		print_f = get_print_func(*(format + i));
 		if (print_f != NULL)
 		{
-			print_f(arg);
-			printf(", ");
+			printf("%s", seperator);
+			print_f(&args);
+			seperator = ", ";
 		}
 		i++;
 	}
 	putchar('\n');
 }
-void (*get_print_func(char ch))(void *)
+/**
+ * get_print_func - gets proper print function for paramater in va_list
+ * @ch: character to get proper function for
+ *
+ * Return: pointer to a function that returns void and takes a paramater of
+ *	   type va_list
+ */
+void (*get_print_func(char ch))(va_list *)
 {
 	int i = 0;
 	conv_t convs[] = {
@@ -54,23 +56,56 @@ void (*get_print_func(char ch))(void *)
 		i++;
 	return (convs[i].f);
 }
-void _print_char(void *ch)
+/**
+ * _print_char - prints a character
+ * @args: va_list to get character from, presumed to be type of arg
+ *
+ * Return: always void
+ */
+void _print_char(va_list *args)
 {
-	printf("%c", *(char *)ch);
+	char ch;
+
+	ch = va_arg(*args, int);
+	printf("%c", ch);
 }
-void _print_integer(void *i)
+/**
+ * _print_integer - prints an integer
+ * @args: va_list to get integer from, presumed to be type of arg
+ *
+ * Return: always void
+ */
+void _print_integer(va_list *args)
 {
-	printf("%d", *(int *)i);
+	int i;
+
+	i = va_arg(*args, int);
+	printf("%d", i);
 }
-void _print_float(void *f)
+/**
+ * _print_float - prints a float
+ * @args: va_list to get float from, presumed to be type of arg
+ *
+ * Return: always void
+ */
+void _print_float(va_list *args)
 {
-	printf("%f", *(float *)f);
+	float f;
+
+	f = va_arg(*args, double);
+	printf("%f", f);
 }
-void _print_string(void *s)
+/**
+ * _print_string - prints a string
+ * @args: va_list to get char * from, presumed to be type of arg
+ *
+ * Return: always void
+ */
+void _print_string(va_list *args)
 {
 	char *str;
 
-	str = s;
+	str = va_arg(*args, char *);
 	if (str == NULL || *str == '\0')
 		printf("(nil)");
 	else
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef _VAR_FUNCS_
 #define _VAR_FUNCS_
+#include <stdarg.h>
+/**
+ * struct print_convert_variable - structure to hold function and char key
+ * @type: abbreviation for type, i=int, c=char, f=float, s=string
+ * @f: pointer to function for proper print statement
+ *
+ * Description: key:value, char:function, e.g: 'c':_print_char
+ */
+typedef struct print_convert_variable
+{
+	char type;
+	void (*f)(va_list *);
+} conv_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* _VAR_FUNCS_ */
