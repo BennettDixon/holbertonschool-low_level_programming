4fb753c9b1ee29c79b73775d2b97a96c2a0ff84d
BennettDixon <bennettdixon16@gmail.com>
2018-10-19 21:53:47 +0000

    printing hello world in assembly, works with ld, but not gcc. Further debugging required

---
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+	global	_start
+	
+	section	.text
+_start:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, message
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
+
+	section	.data
+message:db	"Hello, Holberton", 10
