c96da79867c41919b456d4b575903583149eb12b
BennettDixon <bennettdixon16@gmail.com>
2018-10-20 22:42:05 +0000

    reformatted while loop to check if pointer is null before dereference

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -26,7 +26,7 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(args, format);
-	while ((*(format + i)))
+	while (format && (*(format + i)))
 	{
 		n = 0;
 		while (convs[n].f != NULL && convs[n].type != *(format + i))
