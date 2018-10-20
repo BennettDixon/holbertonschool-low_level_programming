f93fba38e69ea5269e7a0b07effe95539ee4520c
BennettDixon <bennettdixon16@gmail.com>
2018-10-20 22:21:52 +0000

    added _putchar to header file due to holberton requirements

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -17,4 +17,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
 void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(int);
 #endif /* _VAR_FUNCS_ */
