559dded3b52e854aa831ff8aa1f6611b8c16cc0a
BennettDixon <bennettdixon16@gmail.com>
2018-10-25 18:49:20 +0000

    Betty style fixes

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 	}
 	new_node = malloc(sizeof(list_t));
-	if(!new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->len = strlen(str);
 	new_node->str = strdup(str);
