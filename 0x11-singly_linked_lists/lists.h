f8d19cfed4656ef62198828eb0feca6436864277
BennettDixon <bennettdixon16@gmail.com>
2018-10-25 18:54:52 +0000

    betty style fix for header

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -12,13 +12,14 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif /* _LISTS_H_ */
