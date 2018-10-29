7e57dfa88bf3ad882ed4ce3f120e7af41af8e82c
BennettDixon <bennettdixon16@gmail.com>
2018-10-29 17:30:25 +0000

    Fixed betty-documentation

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index - gets a node of linked list at specific index
+ * @head: head of list
+ * @index: index to pull node from
+ *
+ * Return: pointer to node at specified index, NULL if not found
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int ctr = 0;
