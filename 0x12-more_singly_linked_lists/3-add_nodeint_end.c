e92f48834a43618759c2242375ba60ecf6ec20e4
BennettDixon <bennettdixon16@gmail.com>
2018-10-29 16:38:57 +0000

    adding nodes to the end of a linked list

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp_head = NULL, *new_node = NULL;
+
+	temp_head = *head;
+	/* FIRST ELE NOT NULL, advance to last ele of list */
+	if (temp_head)
+	{
+		while (temp_head->next)
+		{
+			temp_head = temp_head->next;
+		}
+	}
+	/* temp_head now pointing to ele before end of list (before NULL ELE */
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (temp_head)
+		temp_head->next = new_node;
+	else
+		*head = new_node;
+	return (temp_head);
+}
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -16,4 +16,6 @@ typedef struct listint_s
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif /* _LISTS_H_ */
