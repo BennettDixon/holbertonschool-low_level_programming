580fd67f310e797d32bc8585269d78e2083b3b95
BennettDixon <bennettdixon16@gmail.com>
2018-10-29 17:43:57 +0000

    Fixed to work with last element of list

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head) /* EMPTY list */
 		return (NULL);
 	temp_h = head;
-	while (temp_h->next)
+	while (temp_h)
 	{
 		if (ctr == index)
 			return (temp_h);
-		temp_h = temp_h->next;
+		if (temp_h->next)
+			temp_h = temp_h->next;
 		ctr++;
 	}
 	return (NULL); /* didn't find node */
