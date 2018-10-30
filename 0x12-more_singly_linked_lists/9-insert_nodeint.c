cc6a296cfcef5a4386f8b73994d221b2af46d837
BennettDixon <bennettdixon16@gmail.com>
2018-10-30 16:04:06 +0000

    Added case for first element insertion

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!*head) /* EMPTY list */
 		return (NULL);
 	temp_h = *head;
+	if (idx == 0) /* insert as first element */
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (!new_node)
+			return (NULL);
+		new_node->n = n; /* set data */
+		new_node->next = temp_h;
+		*head = new_node;
+		return (new_node);
+	}
 	while (temp_h)
 	{
 		if (ctr + 1 == idx) /* insert node here */
