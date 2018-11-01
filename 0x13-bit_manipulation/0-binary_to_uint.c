586d7d768399b4e19537de82554d69cad18316b4
BennettDixon <bennettdixon16@gmail.com>
2018-11-01 16:35:54 +0000

    Converting binary strings to base 10

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts binary string to an unsigned integer
+ * @b: pointer to binary string in memory to convert to integer
+ *
+ * Return: unsigned integer representing binary value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int base = 1;
+	unsigned int temp = 0, result = 0;
+	int len = 0;
+
+	while (b[len])
+		len++;
+	while (len > 0)
+	{
+		temp = (b[len - 1] == '1') ? 1 : 0;
+		result += (temp * base);
+		base *= 2;
+		len--;
+	}
+	return (result);
+}
---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+unsigned int binary_to_uint(const char *b);
+#endif /* _HOLBERTON_H_ */
