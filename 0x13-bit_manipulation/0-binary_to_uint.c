3ac769abc3e8df9b34d5698fff1cc590a9a8ef89
BennettDixon <bennettdixon16@gmail.com>
2018-11-01 19:06:07 +0000

    fixed case of non 0 or 1 in string

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int temp = 0, result = 0;
 	int len = 0;
 
+	if (!b)
+		return (0);
 	while (b[len])
 		len++;
 	while (len > 0)
 	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
 		temp = (b[len - 1] == '1') ? 1 : 0;
 		result += (temp * base);
 		base *= 2;
