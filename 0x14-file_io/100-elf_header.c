9beb9a1609a9a4ff6c7827512755527363fc5c5a
BennettDixon <bennettdixon16@gmail.com>
2018-11-06 22:25:51 +0000

    compiles

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * main - entry point for program to get header of ELF file
  * @argc: count of arguments present in CLI
@@ -8,4 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
 }
