67dbdccf3a9967c74c9d4b7ac7fa196374cd3192
BennettDixon <bennettdixon16@gmail.com>
2018-11-06 21:41:29 +0000

    Fixed file creation to not seg fault on NULL text_content

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	while (*(text_content + len))
+	while (text_content && *(text_content + len))
 		len++;
 	err = write(fd, text_content, len);
 	close(fd);
