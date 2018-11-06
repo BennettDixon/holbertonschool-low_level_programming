367d9201321183aaf82ddc88a41b32f59c808a18
BennettDixon <bennettdixon16@gmail.com>
2018-11-06 18:20:04 +0000

    Creating text files and copying buffers into them with set permissions

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * create_file - creates a file and copies text into it
+ * @filename: the filename to create
+ * @text_content: text to copy into new file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created, or written,
+ * or write fails, etc).
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, err, len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	while (*(text_content + len))
+		len++;
+	err = write(fd, text_content, len);
+	close(fd);
+	if (err < 0)
+		return (-1);
+	return (1);
+}
--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,4 +1,5 @@
 #ifndef __HOLBERTON_H__
 #define __HOLBERTON_H__
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* __HOLBERTON_H__ */
