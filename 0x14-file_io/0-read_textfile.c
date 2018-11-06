69b76a5769f222ade5c61b0b3e395c228c732bd9
BennettDixon <bennettdixon16@gmail.com>
2018-11-06 18:06:56 +0000

    Reading and outputting text files to stdout using system calls

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints the contents to STDOUT
+ * @filename: the filename to open
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters read and printed, 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, err;
+	char *temp_b;
+
+	if (!filename)
+		return (0);
+	temp_b = malloc(sizeof(char) * letters);
+	if (!temp_b)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		free(temp_b);
+		return (0);
+	}
+	err = read(fd, temp_b, letters);
+	if (err < 0)
+	{
+		free(temp_b);
+		return(0);
+	}
+	err = write(STDIN_FILENO, temp_b, err);
+	free(temp_b);
+	close(fd);
+	if (err < 0)
+		return (0);
+	return ((ssize_t)err);
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,4 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* __HOLBERTON_H__ */
