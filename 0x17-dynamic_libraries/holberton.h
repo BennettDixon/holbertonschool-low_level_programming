c5ce5fe28afb7abdc6aa5ef210df54e102a30455
BennettDixon <bennettdixon16@gmail.com>
2018-12-11 00:28:52 +0000

    Include guarded header file

--- 0x17-dynamic_libraries/holberton.h
+++ 0x17-dynamic_libraries/holberton.h
@@ -1,3 +1,5 @@
+#ifndef __HOLBERTON_H__
+#define __HOLBERTON_H__
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif /* __HOLBERTON_H__ */
