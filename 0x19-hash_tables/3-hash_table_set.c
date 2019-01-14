03cb979f84eaa1673a18e661125a053b3aa7e6b8
BennettDixon <bennettdixon16@gmail.com>
2019-01-14 21:14:31 +0000

    added hash_table set method with linked list collision handler

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_set - sets a hash table key value pair in hash table
+ * @key: key to generate hash value and index off
+ * @value: value to set as hash_node's value
+ *
+ * Return: (1) success, (0) failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int key_i = 0;
+	char *val_copy = NULL;
+	hash_node_t *new_node = NULL, *temp_node = NULL;
+
+	if (!ht || !key || !value)
+		return (0);
+	else if (strlen(key) == 0)
+		return (0);
+	val_copy = strdup(value);
+	if (!val_copy)
+		return (0);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = (char *)key;
+	new_node->value = val_copy;
+	new_node->next = NULL;
+
+	key_i = key_index((unsigned char *)key, ht->size);
+	/* COLLISION OCCURED */
+	if ((ht->array)[key_i] != NULL)
+	{
+		temp_node = (ht->array)[key_i];
+		(ht->array)[key_i] = new_node;
+		new_node->next = temp_node;
+	}
+	else
+		(ht->array)[key_i] = new_node;
+	return (1);
+}
