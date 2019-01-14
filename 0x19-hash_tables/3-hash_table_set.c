68e4ce22a8809ec4967c2887658ea641741ae69e
BennettDixon <bennettdixon16@gmail.com>
2019-01-14 22:57:26 +0000

    fixed collision issue with same key, updates value of same key now rather than adding onto list

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int key_i = 0;
-	char *val_copy = NULL;
+	char *val_copy = NULL, *key_copy = NULL;
 	hash_node_t *new_node = NULL, *temp_node = NULL;
 
 	if (!ht || !key || !value)
@@ -23,24 +23,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	val_copy = strdup(value);
 	if (!val_copy)
 		return (0);
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	key_copy = strdup(key);
+	if (!key_copy)
 	{
 		free(val_copy);
 		return (0);
 	}
-	new_node->key = (char *)key;
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(key_copy);
+		free(val_copy);
+		return (0);
+	}
+	new_node->key = key_copy;
 	new_node->value = val_copy;
 	new_node->next = NULL;
-
 	key_i = key_index((unsigned char *)key, ht->size);
 	/* COLLISION OCCURED */
 	if ((ht->array)[key_i] != NULL)
 	{
 		temp_node = (ht->array)[key_i];
-		(ht->array)[key_i] = new_node;
+		while(temp_node)
+		{
+			if (strcmp(temp_node->key, key_copy) == 0)
+			{
+				free(ht->array[key_i]->value);
+				ht->array[key_i]->value = val_copy;
+				free(key_copy);
+				free(new_node);
+				return (1);
+			}
+			temp_node = temp_node->next;
+		}
+		temp_node = (ht->array)[key_i];
 		new_node->next = temp_node;
+		(ht->array)[key_i] = new_node;
 	}
 	else
 		(ht->array)[key_i] = new_node;
