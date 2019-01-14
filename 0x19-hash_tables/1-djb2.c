28ab53b3b821d6288a077848a66e7ef7c5427ac7
BennettDixon <bennettdixon16@gmail.com>
2019-01-14 20:41:25 +0000

    implementing djb2 hash function for hashing

---
+++ 0x19-hash_tables/1-djb2.c
@@ -0,0 +1,12 @@
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
