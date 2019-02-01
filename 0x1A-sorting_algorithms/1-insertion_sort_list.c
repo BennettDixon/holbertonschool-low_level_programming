34e2d463d0d596c45d6810ddb7cf0b982cdd3549
BennettDixon <bennettdixon16@gmail.com>
2019-02-01 21:32:46 +0000

    implementing insertion sort using a doubly linked list!

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,9 +1,77 @@
 #include "sort.h"
 
+/* DEBUG */
+#include <stdio.h>
+
+listint_t *get_list_end(listint_t *list);
+size_t list_len(const listint_t *list);
+
 /**
  * insertion_sort_list - performs insertion sort on a list
  * @list: linked list of ints to sort
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *sorted = *list;
+	listint_t *unsorted, *prev = NULL, *next = NULL, *ele = NULL;
+
+	/* NO LIST, OR LIST IS ONE ELE */
+	if (!list || !(*list) || !((*list)->next))
+		return;
+
+	unsorted = get_list_end(*list);
+	while (sorted && unsorted)
+	{
+		ele = sorted;
+		while (ele->prev && ele->n < ele->prev->n)
+		{
+			/* SWAP ELEMENTS TOWARDS FRONT */
+			prev = ele->prev;
+			next = ele->next;
+			ele->prev = prev->prev;
+			ele->next = prev;
+			if (prev->prev)
+				prev->prev->next = ele;
+			prev->prev = ele;
+			prev->next = next;
+			if (next)
+				next->prev = prev;
+
+			if (!ele->prev) /* reset head if needed */
+				*list = ele;
+			print_list(*list);
+		}
+		sorted = sorted->next;
+	}
+}
+
+/**
+ * list_len - gets the length of a list
+ * @list: list to get length of
+ *
+ * Return: length of list
+ */
+size_t list_len(const listint_t *list)
+{
+	size_t len = 0;
+
+	while (list)
+	{
+		len++;
+		list = list->next;
+	}
+	return len;
+}
+
+/**
+ * get_list_end - gets a pointer to end of linked list
+ * @list: list to get pointer from
+ *
+ * Return: returns pointer to end of list
+ */
+listint_t *get_list_end(listint_t *list)
+{
+	while (list && list->next)
+		list = list->next;
+	return (list);
 }
