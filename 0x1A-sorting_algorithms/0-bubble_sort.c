0047f0961a1db5db5be013a6dbab6c20908f0929
BennettDixon <bennettdixon16@gmail.com>
2019-02-01 19:30:55 +0000

    finished bubble sort implementation, updated Big O to contain best/average/worse

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
-#include <stdlib.h>
+
+void swap(int *array, size_t ind_1, size_t ind_2);
 
 /**
  * bubble_sort - performs bubble sort on an array
@@ -8,4 +9,36 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	BOOL sorted = FALSE;
+	size_t i, sorted_i = size - 1;
+
+	while (!sorted)
+	{
+		sorted = TRUE;
+		for (i = 0; i < sorted_i; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array, i, i + 1);
+				sorted = FALSE;
+				print_array(array, size);
+			}
+		}
+		sorted_i--;
+	}
+}
+
+/**
+ * swap - swaps array index elements
+ * @array: array to swap in
+ * @ind_1: index 1 to swap with index 2
+ * @ind_2: index 2 to swap with index 1
+ */
+void swap(int *array, size_t ind_1, size_t ind_2)
+{
+	int temp;
+
+	temp = array[ind_1];
+	array[ind_1] = array[ind_2];
+	array[ind_2] = temp;
 }
