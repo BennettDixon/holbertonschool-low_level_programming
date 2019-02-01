91cd8ba1268d0ca7eeef11f0f8b1d6b53377d720
BennettDixon <bennettdixon16@gmail.com>
2019-02-01 18:57:41 +0000

    adding sort function declarations to header

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -15,3 +15,9 @@ typedef struct listint_s
 /* PRINT FUNCTIONS */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
+/* SORT FUNCTIONS */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
