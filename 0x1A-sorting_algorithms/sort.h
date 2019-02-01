6c6b2f4ea5e29155d4f27940a017143393e938a8
BennettDixon <bennettdixon16@gmail.com>
2019-02-01 18:10:50 +0000

    header file containing linked list struct def and function defs

---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,17 @@
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* PRINT FUNCTIONS */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
