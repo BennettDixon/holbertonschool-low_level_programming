e66247bed77396c71771b962420668c7a9169449
BennettDixon <bennettdixon16@gmail.com>
2019-02-05 17:34:18 +0000

    adding initial files for deck sorting problem

---
+++ 0x1A-sorting_algorithms/1000-sort_deck.c
@@ -0,0 +1,9 @@
+#include "deck.h"
+
+/**
+ * sort_deck - sorts a deck of cards
+ * @deck: deck of cards to sort
+ */
+void sort_deck(deck_node_t **deck)
+{
+}
---
+++ 0x1A-sorting_algorithms/deck.h
@@ -0,0 +1,43 @@
+#ifndef __DECK__
+#define __DECK__
+
+#include <stdlib.h>
+
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+void sort_deck(deck_node_t **deck);
+
+#endif /* __DECK__ */
