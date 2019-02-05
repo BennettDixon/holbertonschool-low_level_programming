1ddb1eb9d32ce86aadfb68414529fffb2776b142
BennettDixon <bennettdixon16@gmail.com>
2019-02-05 21:15:54 +0000

    added print method, also working swap for any nodes in DLL. Tested with main

--- 0x1A-sorting_algorithms/1000-main.c
+++ 0x1A-sorting_algorithms/1000-main.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 #include "deck.h"
 
-void print_deck(const deck_node_t *deck);
+void print_deck(const deck_node_t *deck)
+{
+    size_t i;
+    char kinds[4] = {'S', 'H', 'C', 'D'};
+
+    i = 0;
+    while (deck)
+    {
+        if (i)
+            printf(", ");
+        printf("{%s, %c}", deck->card->value, kinds[deck->card->kind]);
+        if (i == 12)
+            printf("\n");
+        i = (i + 1) % 13;
+        deck = deck->next;
+    }
+}
 
 deck_node_t *init_deck(const card_t cards[52])
 {
--- 0x1A-sorting_algorithms/1000-sort_deck.c
+++ 0x1A-sorting_algorithms/1000-sort_deck.c
@@ -1,9 +1,61 @@
 #include "deck.h"
 
+void swap_card(deck_node_t *n1, deck_node_t *n2, deck_node_t **deck);
+
 /**
  * sort_deck - sorts a deck of cards
  * @deck: deck of cards to sort
  */
 void sort_deck(deck_node_t **deck)
 {
+	deck_node_t *end = *deck;
+
+	while (end->next)
+		end = end->next;	
+	swap_card((*deck)->next->next, end, deck);
+}
+
+/**
+ * swap_card - swaps to card nodes in a linked list
+ * @n1: node1 to swap with node2
+ * @n2: node2 to swap with node1
+ * @deck: double pointer to deck if head modification is needed
+ */
+void swap_card(deck_node_t *n1, deck_node_t *n2, deck_node_t **deck)
+{
+	deck_node_t *n1_prev, *n1_next, *n2_prev, *n2_next;
+
+	if (!deck || !(*deck) || !n1 || !n2)
+		return;
+	n1_next = n1->next;
+	n1_prev = n1->prev;
+	n2_next = n2->next;
+	n2_prev = n2->prev;
+
+	if (n1_prev != n2)
+		n2->prev = n1_prev;
+	else
+		n2->prev = n1;
+	if (n1_next != n2)
+		n2->next = n1_next;
+	else
+		n2->next = n1;
+	if (n2_prev != n1)
+		n2_prev->next = n1;
+	if (n2_next)
+		n2_next->prev = n1;
+	n1->next = n2_next;
+	if (n2_prev != n1)
+		n1->prev = n2_prev;
+	else
+		n1->prev = n2;
+	if (n1_prev)
+		n1_prev->next = n2;
+	if (n1_next != n2)
+		n1_next->prev = n2;
+
+	if (!n1->prev) /* reset head if needed */
+		*deck = n1;
+	else if (!n2->prev)
+		*deck = n2;
 }
