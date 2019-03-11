2c4b36818526a412454c33d7524cc4d849dc8087
BennettDixon <bennettdixon16@gmail.com>
2019-03-11 14:29:08 +0000

    initial header file with struct definitions

---
+++ 0x1C-binary_trees/binary_trees.h
@@ -0,0 +1,32 @@
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+typedef struct binary_tree_s bst_t;
+
+typedef struct binary_tree_s avl_t;
+
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* __BINARY_TREES_H__
