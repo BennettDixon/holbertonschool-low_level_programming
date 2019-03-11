b03b8727f55b1676739eba497d713ca1f843e67a
BennettDixon <bennettdixon16@gmail.com>
2019-03-11 15:29:02 +0000

    inserting at left of parent and moving children if neccessary

---
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+binary_tree_t *create_empty_node(int value);
+
+/**
+ * binary_tree_insert_left - inserts a node to the left of parent
+ * @parent: parent node to insert child under
+ * @value: value to create new node data
+ *
+ * Return: pointer to new binary tree node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = create_empty_node(value);
+	new_node->parent = parent;
+
+	/* parent contains left node, must be shifted */
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
+
+/**
+ * create_empty_node - creates a node with NULL pointers and a value
+ * @value: value to set as n of node
+ *
+ * Return: newly created orphan node
+ */
+binary_tree_t *create_empty_node(int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -31,4 +31,6 @@ void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
 #endif /* __BINARY_TREES_H__ */
