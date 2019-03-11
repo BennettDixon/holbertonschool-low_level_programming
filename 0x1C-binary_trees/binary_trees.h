57f4c16a231e1045a036c70ae51aa543f4a7b2d4
BennettDixon <bennettdixon16@gmail.com>
2019-03-11 15:14:47 +0000

    added function definitions and fixed a comment error

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -29,4 +29,6 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
-#endif /* __BINARY_TREES_H__
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* __BINARY_TREES_H__ */
