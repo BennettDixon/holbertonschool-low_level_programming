fd6053c9c09ddf443b47729e14f171a186730903
BennettDixon <bennettdixon16@gmail.com>
2019-03-11 15:33:21 +0000

    same as inserting left but swapping to insert at right

---
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_right - inserts a node to the right of parent
+ * @parent: parent node to insert child under
+ * @value: value to create new node data
+ *
+ * Return: pointer to new binary tree node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	/* parent contains left node, must be shifted */
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+
+	return (new_node);
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -33,4 +33,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 #endif /* __BINARY_TREES_H__ */
