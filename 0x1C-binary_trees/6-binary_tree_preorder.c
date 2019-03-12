453105a8039d2c59b8bd120bd2c2adfc6e9aa0df
BennettDixon <bennettdixon16@gmail.com>
2019-03-12 15:10:36 +0000

    added betty-style documentation for function

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_preorder - traverses a tree in pre-order calling func
+ * @tree: const pointer to root node to begin traversal at
+ * @func: function to call on tree node's data
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
