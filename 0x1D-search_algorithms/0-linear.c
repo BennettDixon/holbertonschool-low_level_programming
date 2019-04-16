49b6f517bf8a6c546383691c8d21d7475ee82e86
BennettDixon <bennettdixon16@gmail.com>
2019-04-16 17:52:34 +0000

    header file and linear search with documentation, no code written, include guards on header

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+
+/**
+ * linear_search - linear search implementation for arrays
+ * @array: point to input array
+ * @size: size of array as size_t
+ * @value: value to search for
+ *
+ * Return: index in array where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+}
---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,5 @@
+#ifndef __SEARCH_ALGO_H__
+#define __SEARCH_ALGO_H__
+int linear_search(int *array, size_t size, int value);
+
+#endif /* __SEARCH_ALGO_H__ */
