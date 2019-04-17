3005393bb484c73084767868a394e3254308ff9f
BennettDixon <bennettdixon16@gmail.com>
2019-04-17 21:46:17 +0000

    adding binary_search prototypes and documentation plus base edge cases

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_search - binary search on a sorted array
+ * @array: array to search (sorted)
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: index of value if found, -1 on error or not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (!array || size <= 0)
+		return (-1);
+	/* do binary search */
+	/* TODO */
+
+	/* failed */
+	return (-1);
+}
--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
 #ifndef __SEARCH_ALGO_H__
 #define __SEARCH_ALGO_H__
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 #endif /* __SEARCH_ALGO_H__ */
