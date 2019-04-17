fc31e94877f60d57dab60e9467d57e383630f4d1
BennettDixon <bennettdixon16@gmail.com>
2019-04-17 21:32:09 +0000

    implementing basic linear search

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * linear_search - linear search implementation for arrays
  * @array: point to input array
@@ -9,5 +10,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	int i;
+	int _size = (int)size;
 
+	if (!array || size <= 0)
+		return (-1);
+	for (i = 0; i < _size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
